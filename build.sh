c50c0981b90830b79f1510f68ad81de4a686872c
Charles Suggs <charles@fullsteamlabs.com>
2020-09-04 16:47:03 -0500

    Remove code formatter plugin

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 set -x
 
-docker build --build-arg ETHERPAD_PLUGINS="ep_timesliderdiff ep_markdown ep_code_formatting" --tag charlesfsl/etherpad .
+docker build --build-arg ETHERPAD_PLUGINS="ep_timesliderdiff ep_markdown" --tag charlesfsl/etherpad .
 
 exit 0
