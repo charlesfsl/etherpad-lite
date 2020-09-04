ffdf0311cc9de66824e9b46c7cc1bba67da86099
Charles Suggs <charles@fullsteamlabs.com>
2020-09-04 16:53:50 -0500

    Setup build.sh for code editor setup

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 set -x
 
-docker build --build-arg ETHERPAD_PLUGINS="ep_timesliderdiff ep_markdown" --tag charlesfsl/etherpad .
+docker build --build-arg ETHERPAD_PLUGINS="ep_timesliderdiff ep_markdown ep_codepad" --tag charlesfsl/etherpad:code-editor .
 
 exit 0
