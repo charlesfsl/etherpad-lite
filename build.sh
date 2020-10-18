8b5ade940af2e437b8262a5ed69b57bb9bae983d
Charles Suggs <charles@fullsteamlabs.com>
2020-10-17 19:28:46 -0500

    Add ep_hash_auth plugin

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 set -x
 
-docker build --build-arg ETHERPAD_PLUGINS="ep_timesliderdiff ep_markdown ep_code_formatting" --tag charlesfsl/etherpad .
+docker build --build-arg ETHERPAD_PLUGINS="ep_timesliderdiff ep_markdown ep_code_formatting ep_hash_auth" --tag charlesfsl/etherpad .
 
 exit 0
